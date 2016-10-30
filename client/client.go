d45bcd609a3338a478dc9b923ee03a574a64e763
Travis Jeffery <tj@travisjeffery.com>
2016-10-30 03:14:31 -0500

    add client metadata api

--- client/client.go
+++ client/client.go
@@ -1,20 +1,42 @@
 package client
 
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+
+	"github.com/travisjeffery/jocko/server"
+)
+
+type Options struct {
+	Addr string
+}
+
 type Client struct {
+	Options
+	c *http.Client
 }
 
-func New() *Client {
-	return &Client{}
+func New(options Options) *Client {
+	return &Client{
+		Options: options,
+		c:       &http.Client{},
+	}
 }
 
-func (c *Client) Metadata() {
-
-}
-
-func (c *Client) Produce() {
-
-}
-
-func (c *Client) Fetch() {
-
+func (c *Client) Metadata(topics ...string) (metadata server.MetadataResponse, err error) {
+	req := server.MetadataRequest{Topics: topics}
+	b := new(bytes.Buffer)
+	if err = json.NewEncoder(b).Encode(req); err != nil {
+		return
+	}
+	res, err := c.c.Post(c.Addr+"/metadata", "application/json", b)
+	if err != nil {
+		return metadata, err
+	}
+	defer res.Body.Close()
+	if err := json.NewDecoder(res.Body).Decode(&metadata); err != nil {
+		return metadata, err
+	}
+	return metadata, nil
 }
