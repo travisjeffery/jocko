201c642cfd1736bed254e831f3c3f7287d193f82
Travis Jeffery <tj@travisjeffery.com>
2017-02-12 21:04:09 -0500

    jocko: add some docs

--- jocko.go
+++ jocko.go
@@ -11,6 +11,8 @@ import (
 	"github.com/travisjeffery/jocko/protocol"
 )
 
+// CommitLog is the interface that wraps the commit log's methods and
+// is used to manage a partition's data.
 type CommitLog interface {
 	Init() error
 	Open() error
@@ -22,6 +24,7 @@ type CommitLog interface {
 	Append([]byte) (int64, error)
 }
 
+// Partition is the unit of storage in Jocko.
 type Partition struct {
 	Topic           string  `json:"topic"`
 	ID              int32   `json:"id"`
@@ -36,6 +39,7 @@ type Partition struct {
 	Conn io.ReadWriter `json:"-"`
 }
 
+// NewPartition is used to create a new partition.
 func NewPartition(topic string, id int32) *Partition {
 	return &Partition{
 		ID:    id,
@@ -43,27 +47,36 @@ func NewPartition(topic string, id int32) *Partition {
 	}
 }
 
+// Delete is used to delete the partition's data/commitlog.
 func (p *Partition) Delete() error {
 	return p.CommitLog.DeleteAll()
 }
 
+// NewReader is used to create a reader at the given offset and will
+// read up to maxBytes.
 func (p *Partition) NewReader(offset int64, maxBytes int32) (io.Reader, error) {
 	return p.CommitLog.NewReader(offset, maxBytes)
 }
 
-// Strings returns the topic/Partition as a string.
+// String returns the topic/Partition as a string.
 func (r *Partition) String() string {
 	return fmt.Sprintf("%s/%d", r.Topic, r.ID)
 }
 
+// IsOpen is used to check whether the partition's commit log has been
+// initialized.
 func (r *Partition) IsOpen() bool {
 	return r.CommitLog != nil
 }
 
+// IsLeader is used to check if the given broker ID's the partition's
+// leader.
 func (r *Partition) IsLeader(id int32) bool {
 	return r.Leader == id
 }
 
+// IsFollowing is used to check if the given broker ID's should
+// follow/replicate the leader.
 func (r *Partition) IsFollowing(id int32) bool {
 	for _, b := range r.Replicas {
 		if b == id {
@@ -73,35 +86,43 @@ func (r *Partition) IsFollowing(id int32) bool {
 	return false
 }
 
+// HighWatermark is used to get the newest offset of the partition.
 func (p *Partition) HighWatermark() int64 {
 	return p.CommitLog.NewestOffset()
 }
 
+// LowWatermark is used to oldest offset of the partition.
 func (p *Partition) LowWatermark() int64 {
 	return p.CommitLog.OldestOffset()
 }
 
+// TruncateTo is used to truncate the partition's logs before the given offset.
 func (p *Partition) TruncateTo(offset int64) error {
 	return p.CommitLog.TruncateTo(offset)
 }
 
+// Write is used to directly write the given bytes to the partition's leader.
 func (p *Partition) Write(b []byte) (int, error) {
 	return p.Conn.Write(b)
 }
 
+// Write is used to directly read the given bytes from the partition's leader.
 func (p *Partition) Read(b []byte) (int, error) {
 	return p.Conn.Read(b)
 }
 
+// Append is used to append message sets to the partition.
 func (p *Partition) Append(ms []byte) (int64, error) {
 	return p.CommitLog.Append(ms)
 }
 
+// LeaderID is used to get the partition's leader broker ID.
 func (p *Partition) LeaderID() int32 {
 	return p.Leader
 }
 
-// Serf manages the cluster membership for Jocko nodes
+// Serf is the interface that wraps Serf methods and is used to manage
+// the cluster membership for Jocko nodes.
 type Serf interface {
 	Bootstrap(node *ClusterMember, reconcileCh chan<- *ClusterMember) error
 	Cluster() []*ClusterMember
@@ -119,7 +140,8 @@ type RaftCommand struct {
 	Data *json.RawMessage `json:"data"`
 }
 
-// Raft manages consensus for Jocko cluster
+// Raft is the interface that wraps Raft's methods and is used to
+// manage consensus for the Jocko cluster.
 type Raft interface {
 	Bootstrap(peers []*ClusterMember, fsm raft.FSM, leaderCh chan<- bool) (err error)
 	Apply(cmd RaftCommand) error
@@ -132,6 +154,7 @@ type Raft interface {
 	Addr() string
 }
 
+// Broker is the interface that wraps the Broker's methods.
 type Broker interface {
 	ID() int32
 	IsController() bool
@@ -148,6 +171,8 @@ type Broker interface {
 	IsLeaderOfPartition(topic string, id int32, leaderID int32) bool
 }
 
+// ClusterMember is used as a wrapper around a broker's info and a
+// connection to it.
 type ClusterMember struct {
 	ID   int32  `json:"id"`
 	Port int    `json:"port"`
@@ -160,10 +185,12 @@ type ClusterMember struct {
 	conn net.Conn
 }
 
+// Addr is used to get the address of the member.
 func (b *ClusterMember) Addr() *net.TCPAddr {
 	return &net.TCPAddr{IP: net.ParseIP(b.IP), Port: b.Port}
 }
 
+// Write is used to write the member.
 func (b *ClusterMember) Write(p []byte) (int, error) {
 	if b.conn == nil {
 		if err := b.connect(); err != nil {
@@ -173,6 +200,7 @@ func (b *ClusterMember) Write(p []byte) (int, error) {
 	return b.conn.Write(p)
 }
 
+// Read is used to read from the member.
 func (b *ClusterMember) Read(p []byte) (int, error) {
 	if b.conn == nil {
 		if err := b.connect(); err != nil {
