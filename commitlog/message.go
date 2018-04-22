c0633d57ce45f0825ac7b987a6be855d342481e9
Travis Jeffery <tj@travisjeffery.com>
2018-04-22 06:36:36 -0400

    commitlog: add helpers for getting message fields

--- commitlog/message.go
+++ commitlog/message.go
@@ -5,3 +5,61 @@ type Message []byte
 func NewMessage(p []byte) Message {
 	return Message(p)
 }
+
+func (m Message) Crc() int32 {
+	return int32(Encoding.Uint32(m))
+}
+
+func (m Message) MagicByte() int8 {
+	return int8(m[4])
+}
+
+func (m Message) Attributes() int8 {
+	return int8(m[5])
+}
+
+func (m Message) Timestamp() int64 {
+	if m.MagicByte() < 1 {
+		panic("magic byte < 1")
+	}
+	return int64(Encoding.Uint64(m[6:]))
+}
+
+func (m Message) Key() []byte {
+	start, end, size := m.keyOffsets()
+	if size == -1 {
+		return nil
+	}
+	return m[start+4 : end]
+}
+
+func (m Message) Value() []byte {
+	start, end, size := m.valueOffsets()
+	if size == -1 {
+		return nil
+	}
+	return m[start+4 : end]
+}
+
+func (m Message) keyOffsets() (start, end, size int32) {
+	if m.MagicByte() == 0 {
+		start = 6
+	} else {
+		start = 14
+	}
+	size = int32(Encoding.Uint32(m[start:]))
+	end = start + 4 + size
+	return
+}
+
+func (m Message) valueOffsets() (start, end, size int32) {
+	keyStart, keyEnd, keySize := m.keyOffsets()
+	if keySize == -1 {
+		start = keyStart + 4
+	} else {
+		start = keyEnd + 1
+	}
+	size = int32(Encoding.Uint32(m[start:]))
+	end = start + size
+	return
+}
---
+++ commitlog/message_test.go
@@ -0,0 +1,41 @@
+package commitlog
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	emptyMessage = []byte{
+		167, 236, 104, 3, // CRC
+		0x00,                   // magic version byte
+		0x00,                   // attribute flags
+		0xFF, 0xFF, 0xFF, 0xFF, // key
+		0xFF, 0xFF, 0xFF, 0xFF} // value
+
+	emptyV1Message = []byte{
+		204, 47, 121, 217, // CRC
+		0x01,                                           // magic version byte
+		0x00,                                           // attribute flags
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // timestamp
+		0xFF, 0xFF, 0xFF, 0xFF, // key
+		0xFF, 0xFF, 0xFF, 0xFF} // value
+
+	// TODO: v2
+
+)
+
+func TestMessage(t *testing.T) {
+	req := require.New(t)
+
+	m := Message(emptyMessage)
+	req.Equal(int8(0), m.MagicByte())
+	req.Nil(m.Key())
+	req.Nil(m.Value())
+
+	m = Message(emptyV1Message)
+	req.Equal(int8(1), m.MagicByte())
+	req.Nil(m.Key())
+	req.Nil(m.Value())
+}
