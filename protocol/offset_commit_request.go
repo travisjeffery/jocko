92fa505dbd3277ef218ce44f5b14c33f2fc43273
Travis Jeffery <tj@travisjeffery.com>
2018-04-28 06:37:38 -0400

    protocol: implement offset commit

--- protocol/offset_commit_request.go
+++ protocol/offset_commit_request.go
@@ -1,11 +1,100 @@
 package protocol
 
-type OffsetCommitRequest struct{}
+type OffsetCommitRequest struct {
+	APIVersion int16
+
+	GroupID       string
+	GenerationID  int32
+	MemberID      string
+	RetentionTime int64
+	Topics        []OffsetCommitTopicRequest
+}
+
+type OffsetCommitTopicRequest struct {
+	Topic      string
+	Partitions []OffsetCommitPartitionRequest
+}
+
+type OffsetCommitPartitionRequest struct {
+	Partition int32
+	Offset    int64
+	Timestamp int64
+	Metadata  *string
+}
 
 func (r *OffsetCommitRequest) Encode(e PacketEncoder) (err error) {
+	if err = e.PutString(r.GroupID); err != nil {
+		return err
+	}
+	if err := e.PutArrayLength(len(r.Topics)); err != nil {
+		return err
+	}
+	for _, t := range r.Topics {
+		if err := e.PutArrayLength(len(t.Partitions)); err != nil {
+			return err
+		}
+		for _, p := range t.Partitions {
+			e.PutInt32(p.Partition)
+			e.PutInt64(p.Offset)
+			if err := e.PutNullableString(p.Metadata); err != nil {
+				return err
+			}
+		}
+	}
 	return nil
 }
 
 func (r *OffsetCommitRequest) Decode(d PacketDecoder, version int16) (err error) {
+	r.APIVersion = version
+
+	if r.GroupID, err = d.String(); err != nil {
+		return err
+	}
+	if version >= 1 {
+		if r.GenerationID, err = d.Int32(); err != nil {
+			return err
+		}
+		if r.MemberID, err = d.String(); err != nil {
+			return err
+		}
+	}
+	if version >= 2 {
+		if r.RetentionTime, err = d.Int64(); err != nil {
+			return err
+		}
+	}
+	topicCount, err := d.ArrayLength()
+	if err != nil {
+		return err
+	}
+	r.Topics = make([]OffsetCommitTopicRequest, topicCount)
+	for _, t := range r.Topics {
+		partitionCount, err := d.ArrayLength()
+		if err != nil {
+			return err
+		}
+		t.Partitions = make([]OffsetCommitPartitionRequest, partitionCount)
+		for _, p := range t.Partitions {
+			if p.Partition, err = d.Int32(); err != nil {
+				return err
+			}
+			if p.Offset, err = d.Int64(); err != nil {
+				return err
+			}
+			if version >= 1 {
+				if p.Timestamp, err = d.Int64(); err != nil {
+					return err
+				}
+			}
+			if p.Metadata, err = d.NullableString(); err != nil {
+				return err
+			}
+		}
+	}
 	return nil
+}
+
+func (r *OffsetCommitRequest) Version() int16 {
+	return r.APIVersion
+
 }
--- protocol/offset_commit_response.go
+++ protocol/offset_commit_response.go
@@ -1,11 +1,82 @@
 package protocol
 
-type OffsetCommitResponse struct{}
+import "time"
+
+type OffsetCommitResponse struct {
+	APIVersion int16
+
+	ThrottleTime time.Duration
+	Responses    []OffsetCommitTopicResponse
+}
+
+type OffsetCommitTopicResponse struct {
+	Topic              string
+	PartitionResponses []OffsetCommitPartitionResponse
+}
+
+type OffsetCommitPartitionResponse struct {
+	Partition int32
+	ErrorCode int16
+}
 
 func (r *OffsetCommitResponse) Encode(e PacketEncoder) (err error) {
+	if r.APIVersion >= 3 {
+		e.PutInt32(int32(r.ThrottleTime / time.Millisecond))
+	}
+	if err := e.PutArrayLength(len(r.Responses)); err != nil {
+		return err
+	}
+	for _, t := range r.Responses {
+		if err := e.PutString(t.Topic); err != nil {
+			return err
+		}
+		if err := e.PutArrayLength(len(t.PartitionResponses)); err != nil {
+			return err
+		}
+		for _, p := range t.PartitionResponses {
+			e.PutInt32(p.Partition)
+			e.PutInt16(p.ErrorCode)
+		}
+	}
 	return nil
 }
 
 func (r *OffsetCommitResponse) Decode(d PacketDecoder, version int16) (err error) {
+	r.APIVersion = version
+
+	if version >= 3 {
+		throttle, err := d.Int32()
+		if err != nil {
+			return err
+		}
+		r.ThrottleTime = time.Duration(throttle) * time.Millisecond
+	}
+
+	topicCount, err := d.ArrayLength()
+	if err != nil {
+		return err
+	}
+	r.Responses = make([]OffsetCommitTopicResponse, topicCount)
+	for _, t := range r.Responses {
+		if t.Topic, err = d.String(); err != nil {
+			return err
+		}
+		partitionCount, err := d.ArrayLength()
+		if err != nil {
+			return err
+		}
+		t.PartitionResponses = make([]OffsetCommitPartitionResponse, partitionCount)
+		for _, p := range t.PartitionResponses {
+			p.Partition, err = d.Int32()
+			if err != nil {
+				return err
+			}
+			p.ErrorCode, err = d.Int16()
+			if err != nil {
+				return err
+			}
+		}
+
+	}
 	return nil
 }
