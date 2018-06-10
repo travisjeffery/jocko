efbc1ccab970a20c1c02459db55c8fda7210c0b0
Travis Jeffery <tj@travisjeffery.com>
2018-06-10 04:21:09 -0500

    refactor ctx

--- jocko/context.go
+++ jocko/context.go
@@ -11,13 +11,25 @@ import (
 
 type Context struct {
 	sync.Mutex
-	Conn     io.ReadWriter
-	Error    error
-	Header   *protocol.RequestHeader
-	Parent   context.Context
-	Request  interface{}
-	Response interface{}
-	values   map[interface{}]interface{}
+	conn   io.ReadWriter
+	err    error
+	header *protocol.RequestHeader
+	parent context.Context
+	req    interface{}
+	res    interface{}
+	vals   map[interface{}]interface{}
+}
+
+func (ctx *Context) Request() interface{} {
+	return ctx.req
+}
+
+func (ctx *Context) Response() interface{} {
+	return ctx.res
+}
+
+func (c *Context) Header() *protocol.RequestHeader {
+	return c.header
 }
 
 func (ctx *Context) Deadline() (deadline time.Time, ok bool) {
@@ -29,17 +41,17 @@ func (ctx *Context) Done() <-chan struct{} {
 }
 
 func (ctx *Context) Err() error {
-	return ctx.Error
+	return ctx.err
 }
 
 func (ctx *Context) Value(key interface{}) interface{} {
 	ctx.Lock()
-	if ctx.values == nil {
-		ctx.values = make(map[interface{}]interface{})
+	if ctx.vals == nil {
+		ctx.vals = make(map[interface{}]interface{})
 	}
-	val := ctx.values[key]
+	val := ctx.vals[key]
 	if val == nil {
-		val = ctx.Parent.Value(key)
+		val = ctx.parent.Value(key)
 	}
 	ctx.Unlock()
 	return val
