edb1a739894685fa3f1ccf41038a9fabf5246f2d
Travis Jeffery <tj@travisjeffery.com>
2018-07-19 04:41:45 -0400

    protocol: implement protocol.Body for few reqs

--- protocol/sasl_handshake_request.go
+++ protocol/sasl_handshake_request.go
@@ -1,6 +1,8 @@
 package protocol
 
-type SaslHandshakeRequest struct{}
+type SaslHandshakeRequest struct {
+	APIVersion int16
+}
 
 func (r *SaslHandshakeRequest) Encode(e PacketEncoder) (err error) {
 	return nil
@@ -8,4 +10,12 @@ func (r *SaslHandshakeRequest) Encode(e PacketEncoder) (err error) {
 
 func (r *SaslHandshakeRequest) Decode(d PacketDecoder, version int16) (err error) {
 	return nil
+}
+
+func (r *SaslHandshakeRequest) Key() int16 {
+	return SaslHandshakeKey
+}
+
+func (r *SaslHandshakeRequest) Version() int16 {
+	return r.APIVersion
 }
